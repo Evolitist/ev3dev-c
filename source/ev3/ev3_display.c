33d46a5b70caa976302479173cfb00ba649941c1
Evolitist <evolitist@gmail.com>
2018-08-22 14:07:15 +0700

    Unbreak display module on Stretch
    NOT COMPATIBLE WITH JESSIE ANYMORE

--- source/ev3/ev3_display.c
+++ source/ev3/ev3_display.c
@@ -15,10 +15,10 @@
 #include "linux/kd.h"
 #include "system.8x16.h"
 
-#define LCD_X_SIZE     178
-#define LCD_Y_SIZE     128
-#define LCD_LINE_SIZE   24
-#define LCD_FB_LENGTH 3072
+#define LCD_X_SIZE		178
+#define LCD_Y_SIZE		128
+#define LCD_LINE_SIZE		712
+#define LCD_FB_LENGTH		91136
 
 int fb_file = 0, vt_file = 0;
 uint8_t *fb_ptr = NULL, old_fb[LCD_FB_LENGTH];
@@ -34,28 +34,40 @@ int ev3_display_init()
 	ioctl(vt_file, KDSETMODE, KD_GRAPHICS);
 	setlocale(LC_ALL, "en_US.utf8");
 	memcpy(old_fb, fb_ptr, LCD_FB_LENGTH);
-	memset(fb_ptr, 0, LCD_FB_LENGTH);
+	memset(fb_ptr, 255, LCD_FB_LENGTH);
 	return 0;
 }
 
 void display_clear()
 {
-	memset(fb_ptr, 0, LCD_FB_LENGTH);
+	memset(fb_ptr, 255, LCD_FB_LENGTH);
 }
 
 void display_draw_pixel(int x, int y, bool fill)
 {
 	if(x < 0 || x >= LCD_X_SIZE || y < 0 || y >= LCD_Y_SIZE) return;
-	int trgt = y * 24 + (x >> 3);
-	uint8_t b = 1 << (x & 7);
-	memset(fb_ptr + trgt, fill ? fb_ptr[trgt] | b : fb_ptr[trgt] & (0xFF ^ b), 1);
+	int trgt = y * LCD_LINE_SIZE + (x * 4);
+	memset(fb_ptr + trgt, fill ? 0 : 255, 4);
 }
 
 void display_clear_text_line(uint8_t line)
 {
 	if(line >= 8) return;
-	int sl = line * 16 * 24;
-	memset(fb_ptr + sl, 0, 384);
+	int sl = line * 16 * LCD_LINE_SIZE;
+	memset(fb_ptr + sl, 255, LCD_LINE_SIZE);
+}
+
+void draw_char_at(int _sl, char toDraw)
+{
+	int sl = _sl;
+	char t = toDraw ^ 255, st;
+	while(t != 0)
+	{
+		st = (t & 1) * 255;
+		memset(fb_ptr + sl, st, 4);
+		sl += 4;
+		t = t >> 1;
+	}
 }
 
 void display_draw_text_line(uint8_t line, const char *__str, ...)
@@ -68,7 +80,7 @@ void display_draw_text_line(uint8_t line, const char *__str, ...)
 	va_end(vl);
 	wchar_t str[23];
 	mbstowcs(str, _str, 22);
-	int sl = line * 16 * 24;
+	int sl = line * 16 * LCD_LINE_SIZE;
 	bool wipe;
 	for(int i = 0; i < 16; i++)
 	{
@@ -78,13 +90,18 @@ void display_draw_text_line(uint8_t line, const char *__str, ...)
 			if(str[j] == 0) wipe = true;
 			if(wipe)
 			{
-				memset(fb_ptr + sl, 0, 24 - (sl % 24));
+				memset(fb_ptr + sl, 255, LCD_LINE_SIZE - (sl % LCD_LINE_SIZE));
 				break;
 			}
-			else memset(fb_ptr + sl, get_char((int)str[j])[i], 1);
-			sl++;
+			else
+			{
+				draw_char_at(sl, get_char((int)str[j])[i]);
+				sl += 32;
+				//memset(fb_ptr + sl, get_char((int)str[j])[i], 1);
+			}
+			//sl += 32;
 		}
-		sl += 24 - (sl % 24);
+		sl += LCD_LINE_SIZE - (sl % LCD_LINE_SIZE);
 	}
 }
 
@@ -99,26 +116,24 @@ void display_draw_centered_text_line(uint8_t line, const char *__str, ...)
 	wchar_t str[23];
 	mbstowcs(str, _str, 22);
 	int wsl = wcslen(str);
-	int sl = line * 16 * 24, sp = sl + (22 - wsl) / 2;
-	bool shift = (wsl % 2 != 0);
+	int sl = line * 16 * LCD_LINE_SIZE, sp = sl + ((22 - wsl) / 2 * 32);
+	if(wsl % 2 != 0)
+	{
+		str[wsl] = L' ';
+		str[wsl + 1] = 0;
+		wsl++;
+	}
 	for(int i = 0; i < 16; i++)
 	{
-		memset(fb_ptr + sl, 0, sp - sl);
-		if(shift) memset(fb_ptr + sp, (get_char((int)str[0])[i] & 0x0F) << 4, 1);
-		else memset(fb_ptr + sp, get_char((int)str[0])[i], 1);
-		for(int j = 1; j < wsl; j++)
+		memset(fb_ptr + sl, 255, sp - sl);
+		//draw_char_at(sp, get_char((int)str[0])[i]);
+		for(int j = 0; j < wsl; j++)
 		{
-			if(shift)
-			{
-				memset(fb_ptr + sp + j, ((get_char((int)str[j])[i] & 0x0F) << 4) + (get_char((int)str[j-1])[i] >> 4), 1);
-			}
-			else memset(fb_ptr + sp + j, get_char((int)str[j])[i], 1);
+			draw_char_at(sp + j * 32, get_char((int)str[j])[i]);
 		}
-		if(shift) memset(fb_ptr + sp + wsl, get_char((int)str[wsl - 1])[i] >> 4, 1);
-		else memset(fb_ptr + sp + wsl, 0, 1);
-		memset(fb_ptr + sp + wsl + 1, 0, 24 - ((sp + wsl + 1) % 24));
-		sl += 24;
-		sp += 24;
+		memset(fb_ptr + sp + wsl * 32, 255, LCD_LINE_SIZE - ((sp + wsl * 32) % LCD_LINE_SIZE));
+		sl += LCD_LINE_SIZE;
+		sp += LCD_LINE_SIZE;
 	}
 }
 
@@ -132,16 +147,16 @@ void display_draw_string(uint8_t line, const char *__str, ...)
 	va_end(vl);
 	wchar_t str[23];
 	mbstowcs(str, _str, 22);
-	int sl = line * 16 * 24;
+	int sl = line * 16 * LCD_LINE_SIZE;
 	for(int i = 0; i < 16; i++)
 	{
 		for(int j = 0; j < 22; j++)
 		{
 			if(str[j] == 0) break;
-			memset(fb_ptr + sl, get_char((int)str[j])[i], 1);
-			sl++;
+			draw_char_at(sl, get_char((int)str[j])[i]);
+			sl += 32;
 		}
-		sl += 24 - (sl % 24);
+		sl += LCD_LINE_SIZE - (sl % LCD_LINE_SIZE);
 	}
 }
 
