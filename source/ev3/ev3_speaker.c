985fca8d9e861a95bf249d638cd540247e2e7663
Evolitist <evolitist@gmail.com>
2018-11-08 11:48:11 +0700

    Attempt to fix sound on ev3dev-stretch

--- source/ev3/ev3_speaker.c
+++ source/ev3/ev3_speaker.c
@@ -1,32 +1,34 @@
 #include "ev3_speaker.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
-#include "ev3.h"
+//#include "ev3.h"
 
-#define PATH_TONE   "/sys/devices/platform/snd-legoev3/tone"
-#define PATH_VOLUME "/sys/devices/platform/snd-legoev3/volume"
+//#define PATH_TONE   "/sys/devices/platform/snd-legoev3/tone"
+//#define PATH_VOLUME "/sys/devices/platform/snd-legoev3/volume"
 
 void speaker_play_tone(int freq, int timeMs)
 {
-	if(freq < 30 || freq >= 8000 || timeMs <= 0) return;
-	char path[] = PATH_TONE;
-	char str[17];
-	sprintf(str, "%d %d", freq, timeMs);
-	ev3_write_char_array(path, str);
+	if(freq < 30 || freq > 5000 || timeMs <= 0 || timeMs > 9999) return;
+	char cmd[30] = {0};
+	sprintf(cmd, "/usr/bin/beep -f %d -l %d", freq, timeMs);
+	popen(cmd, "r");
 }
 
 void speaker_set_volume(int vol)
 {
-	if(vol < 0 || vol > 100) return;
-	char path[] = PATH_VOLUME;
-	ev3_write_int(path, vol);
+	//if(vol < 0 || vol > 100) return;
+	//char path[] = PATH_VOLUME;
+	//ev3_write_int(path, vol);
+	//TODO: implement
 }
 
 int speaker_get_volume()
 {
-	char path[] = PATH_VOLUME;
-	int buf = 0;
-	ev3_read_int(path, &buf);
-	return buf;
+	//char path[] = PATH_VOLUME;
+	//int buf = 0;
+	//ev3_read_int(path, &buf);
+	//TODO: implement
+	return 100;
 }
